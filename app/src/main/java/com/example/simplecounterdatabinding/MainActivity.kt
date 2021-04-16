2906f398d4dfcbc66988f9badbc555cea9103abe
ĐỨC NGUYỄN HỒNG <18120323@student.hcmus.edu.vn>
2021-04-16 18:26:13 +0700

    Fix: ViewModelProvider; Refresh button

--- app/src/main/java/com/example/simplecounterdatabinding/MainActivity.kt
+++ app/src/main/java/com/example/simplecounterdatabinding/MainActivity.kt
@@ -2,27 +2,45 @@ package com.example.simplecounterdatabinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.simplecounterdatabinding.databinding.ActivityMainBinding
 import com.example.simplecounterdatabinding.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         val dataBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         dataBinding.apply {
             this.lifecycleOwner = this@MainActivity
             this.viewmodel = mainViewModel
         }
+    }
 
-//        mainViewModel.currentCount.observe(this, Observer {
-//
-//        })
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if(id == R.id.refresh)
+        {
+            mainViewModel.onResetBtnClick()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
